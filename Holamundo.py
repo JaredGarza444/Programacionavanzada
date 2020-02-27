d951f4f6bbd605e0e8fbf07ace3deb6920b1b086
JaredGarza444 <61489417+JaredGarza444@users.noreply.github.com>
2020-02-27 11:42:21 -0600

    Add files via upload
    Ejercicios de programacion para el repaso de funciones pertinentes en Python

---
+++ Holamundo.py
@@ -0,0 +1,5 @@
+#La funcion print permite imprimir 
+#Un mensaje conocido o definido 
+#previamente 
+
+print ("Hola mundo")
---
+++ main.py
@@ -0,0 +1,15 @@
+print ("Ejercicio 1...")
+print ("Hola mundo")
+
+print ("Ejercicio 2...")
+texto=("Estoy intentando programar")
+print(texto)
+
+print ("Ejercicio 3...")
+intLado1=15
+intLado2=10
+AreaCuadrado=(intLado1*intLado2)/2
+texto="AreaCuadrado:{2:0.2f}({0} por {1} entre dos)"
+print(texto.format(intLado1,intLado2,AreaCuadrado))
+
+print ("Ejercicio 4...")
